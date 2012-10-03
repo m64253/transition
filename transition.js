da67fa6c8b715a9382520f661dde7a1d028af32d
Mikael Abrahamsson <mikael.abrahamsson@gmail.com>
2012-10-03 16:13:02 +0200

    Added require support

--- transition.js
+++ transition.js
@@ -1,4 +1,5 @@
-/*jshint smarttabs: true*/
+/*jshint smarttabs: true */
+/*globals module, setTimeout, clearTimeout */
 (function (root) {
 	"use strict";
 	
@@ -18,9 +19,9 @@
 		 * @return {HTMLElement}
 		 */
 		transition = root.transition = function (el, css, options) {
-
+			
 			options = options || {};
-
+			
 			var onTransitionEnd = function (e) {
 					if (e.target === el) {
 						el.removeEventListener(transition.eventName, onTransitionEnd, false);
@@ -71,43 +72,49 @@
 	transition.propertyName = null;
 	transition.durationName = null;
 	transition.eventName = null;
-	
-	(function () {
-		var testEl = doc.createElement('div'),
-			stylePrefixes = {
-				'webkit'	: 'webkitTransition',
-				'moz'		: 'MozTransition',
-				'o'			: 'OTransition',
-				'ms'		: 'msTransition',
-				''			: 'transition'
-			},
-			events = {
-				'webkit'	: 'webkitTransitionEnd',
-				'moz'		: 'transitionend',
-				'o'			: 'OTransitionEnd',
-				'ms'		: 'msTransitionEnd',
-				''			: 'transitionend'
-			},
-			styleProperty,
-			vendor;
-		
-		for (vendor in stylePrefixes) {
-			if (stylePrefixes.hasOwnProperty(vendor)) {
-				styleProperty = stylePrefixes[vendor];
-				
-				if (testEl.style[styleProperty] !== undefined) {
-					transition.supported = true;
+
+	// 
+	if (typeof module !== 'undefined' && module.exports) {
+		module.transition = transition;
+	} else {
+		(function () {
+			var testEl = doc.createElement('div'),
+				stylePrefixes = {
+					'webkit'	: 'webkitTransition',
+					'moz'		: 'MozTransition',
+					'o'			: 'OTransition',
+					'ms'		: 'msTransition',
+					''			: 'transition'
+				},
+				events = {
+					'webkit'	: 'webkitTransitionEnd',
+					'moz'		: 'transitionend',
+					'o'			: 'OTransitionEnd',
+					'ms'		: 'msTransitionEnd',
+					''			: 'transitionend'
+				},
+				styleProperty,
+				vendor;
+			
+			for (vendor in stylePrefixes) {
+				if (stylePrefixes.hasOwnProperty(vendor)) {
+					styleProperty = stylePrefixes[vendor];
 					
-					transition.propertyName = styleProperty + 'Property';
-					transition.durationName = styleProperty + 'Duration';
-					transition.easingName = styleProperty + 'TimingFunction';
-					transition.delayName = styleProperty + 'Delay';
-					
-					transition.eventName = events[vendor];
-					
-					break;
+					if (testEl.style[styleProperty] !== undefined) {
+						transition.supported = true;
+						
+						transition.propertyName = styleProperty + 'Property';
+						transition.durationName = styleProperty + 'Duration';
+						transition.easingName = styleProperty + 'TimingFunction';
+						transition.delayName = styleProperty + 'Delay';
+						
+						transition.eventName = events[vendor];
+						
+						break;
+					}
 				}
 			}
-		}
-	}());
+		}());
+		root.transition = transition;
+	}
 }(this));
