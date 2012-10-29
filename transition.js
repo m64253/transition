2543b24920778d7348b09d0e940e2575e1ee64cd
Mikael Abrahamsson <mikael.abrahamsson@gmail.com>
2012-10-29 08:37:25 +0100

    Added transform

--- transition.js
+++ transition.js
@@ -2,37 +2,36 @@
 /*globals module, setTimeout, clearTimeout */
 (function (root) {
 	"use strict";
-
+	
+	
 	var win = root.window,
 		doc = win.document,
 		setTimeout = win.setTimeout,
-
-
+		
+		
 		/**
 		 * Poor mans cross browser transition function
 		 *
-		 * @static
-		 * @class transition
 		 * @param {HTMLElement} el
 		 * @param {Object} css
 		 * @params {Object} [options]
 		 * @return {HTMLElement}
 		 */
-			transition = root.transition = function (el, css, options) {
-
+		transition = root.transition = function (el, css, options) {
+			
 			options = options || {};
-
+			
 			var onTransitionEnd = function (e) {
 					if (e.target === el) {
 						el.removeEventListener(transition.eventName, onTransitionEnd, false);
-
+						
 						el.style[transition.transform] = null;
-
+						
 						el.style[transition.propertyName] = null;
 						el.style[transition.durationName] = null;
 						el.style[transition.delayName] = null;
 						el.style[transition.easingName] = null;
-
+						
 						if (options.callback) {
 							options.callback();
 						}
@@ -40,44 +39,70 @@
 				},
 				property,
 				properties = [];
-
-
+			
+			
 			if (transition.supported) {
 				for (property in css) {
 					if (css.hasOwnProperty(property)) {
 						properties.push(property.replace(/([A-Z])/g, '-$1').toLowerCase());
 					}
 				}
-
-				el.style[transition.transform] = 'translate3d(0, 0, 0)';
-
+				
+				if (!css.hasOwnProperty(transition.transformPropertyName)) {
+					el.style[transition.transformPropertyName] = 'translate3d(0, 0, 0)';
+				}
+				
 				el.style[transition.propertyName] = properties.join(', ');
 				el.style[transition.durationName] = (options.duration || 1000) + 'ms';
 				el.style[transition.delayName] = (options.delay || 0) + 'ms';
 				el.style[transition.easingName] = options.easing || 'linear';
-
+				
 				el.addEventListener(transition.eventName, onTransitionEnd, false);
-
+				
 			} else if (options.callback) {
 				setTimeout(options.callback, 0);
 			}
-
-
+			
+			
 			for (property in css) {
 				if (css.hasOwnProperty(property)) {
 					el.style[property] = css[property];
 				}
 			}
-
+			
 			return el;
 		};
-
+	
+	
+	/**
+	 * Do a transform translate3d transition
+	 * 
+	 * @param {HTMLElement} el
+	 * @param {Array} value
+	 * @param {Object} [options]
+	 */
+	transition.transform = function (el, value, options) {
+		var css = {};
+		
+		if (transition.supported) {
+			css[transition.transformPropertyName] = 'translate3d(' + value.join(', ') + ')';
+		} else {
+			css.left = value[0];
+			css.top = value[1];
+		}
+		
+		return transition(el, css, options);
+	};
+	
+	
 	transition.supported = false;
 	transition.propertyName = null;
 	transition.durationName = null;
 	transition.eventName = null;
-
-	// 
+	transition.transformPropertyName = null;
+	
+	
+	// Test what is supported
 	if (typeof module !== 'undefined' && module.exports) {
 		module.transition = transition;
 	} else {
@@ -99,24 +124,24 @@
 				},
 				styleProperty,
 				vendor;
-
+			
 			for (vendor in stylePrefixes) {
 				if (stylePrefixes.hasOwnProperty(vendor)) {
 					styleProperty = stylePrefixes[vendor];
-
+					
 					if (testEl.style[styleProperty] !== undefined) {
 						transition.supported = true;
-
+						
 						transition.propertyName = styleProperty + 'Property';
 						transition.durationName = styleProperty + 'Duration';
 						transition.easingName = styleProperty + 'TimingFunction';
 						transition.delayName = styleProperty + 'Delay';
-
+						
 						transition.eventName = events[vendor];
 
-						transition.transform = vendor ? vendor + 'Transform' : 'transform';
-
-
+						transition.transformPropertyName = vendor ? vendor + 'Transform' : 'transform';
+						
+						
 						break;
 					}
 				}
